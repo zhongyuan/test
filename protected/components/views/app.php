b5f594d47ddce0441b4aa19fec8cc82d80c18541
zhongyuan <910398301@qq.com>
2013-04-03 17:38:47 +0800

    应用开发大赛

--- protected/components/views/app.php
+++ protected/components/views/app.php
@@ -4,33 +4,59 @@
     .app_bg{
         width: 1100px;
         height: 321px;
+        color: white;
         background-image: url('/cos/images/appIndex/app.jpg');
     }
+    .app_title{
+        position: relative;
+        width: 350px;
+        height: 140px;
+        left: 570px;
+        top: 90px;
+        font-size: 55px;
+        font-weight: bolder;
+        text-align: right;
+        line-height: 52px;
+    }
+    .app_title span{
+        font-size: 37px;
+        font-family: ddd;
+    }
+    .app_nav{
+        position: relative;
+        width: 550px;
+        height: 30px;
+        left: 70px;
+        top: 150px;
+    }
+    .app_nav span{
+        padding: 18px;
+    }
+
+    .app_parti{
+        position: relative;
+        top: 103px;
+        left: 810px;
+        color: green;
+        font-size: 25px;
+    }
+
 </style>
 
-<div>
+<!-- 样式命名规则 -->
+
+
     <div class="app_bg">
-        <p>2013COS</p>
-    </div>
-    <p class="weizi">
-
-    </p>
-    <div class ="henxian"></div>
-    <div>
-        <div>
-
-        </div>
-
-        <div>
-            <ul>
-                <li>
-
-                </li>
-            </ul>
-        </div>
-
+        <p class="app_title"><span>2013</span>COS<br/>应用开发大赛</p>
+        <p class="app_nav">
+            <span>大赛介绍</span>
+            <span>参赛详情</span>
+            <span>报名参加</span>
+            <span>作品展示</span>
+            <span>获奖名单</span>
+        </p>
+        <p class="app_parti">我要参赛</p>
     </div>
 
 
 
-</div>
--- protected/views/news/appIndex.php
+++ protected/views/news/appIndex.php
@@ -6,3 +6,71 @@
  */
 ?>
 <?php $this->widget('AppWidget');?>
+
+<style>
+    .app_ibody{
+        padding: 0px 70px;
+        color: gray;
+        /*先设置为灰色*/
+    }
+    .app_para{
+        padding: 20px 0px;
+        font-size: 14px;
+    }
+    .horizon_line{
+        overflow:hidden;
+        height: 1px;
+        width: 100%;
+        background-color: #c2c4c8;
+    }
+    .vertical_line{
+        height: 10px;
+        width: 3px;
+        background-color: #ff6633;
+        /*颜色未定*/
+    }
+    .app_ileft{
+
+    }
+    .app_iprocess{
+        font-size: 24px;
+        color: black;
+        margin: 0px 20px;
+    }
+</style>
+
+<div class="app_ibody">
+    <p class="app_para">经过多年的努力！终于，中国拥有了自己的OS！新的设计，也在人与人之间。洞
+    悉该模式的团队会早早的对付接口，在提交所有的组件代码之前，他们构建了一系列的程序来检验接口。
+    他们早早地集成个人代码，频繁地测试。请记住康威定律(Conway's Law):产品反映了制造该产品的
+    组织结构。对于接口，这一点尤为正确：项目中易导致复杂的产品接口</p>
+
+    <div class="horizon_line"></div>
+
+    <div class="app_ileft">
+        <div class="app_iprocess">
+            <span class="vertical_line">&nbsp;</span><span class="app_iprocess">大赛流程</span>
+            <p>
+                <img src="<?php echo $this->staticUrl('appIndex/app_iprocess.jpg');?>" />
+            </p>
+        </div>
+
+        <div>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="app_iright">
+        <!--<span class="vertical_line">&nbsp;</span><span>大赛流程</span>-->
+        <ul>
+
+        </ul>
+    </div>
+
+
+</div>
