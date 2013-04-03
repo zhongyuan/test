baa9069982fe433e478bd59e19ae19a4983c8fe2
zhongyuan <910398301@qq.com>
2013-04-03 14:51:39 +0800

    应用开发大赛

--- protected/components/AppWidget.php
+++ protected/components/AppWidget.php
@@ -4,4 +4,23 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+class AppWidget extends CWidget {
+
+    public function run()
+    {
+        $action =  Yii::app()->getController()->getAction()->getId();
+
+        $this->render('app',array(
+            'action' => $action,
+        ));
+    }
+
+}
+
+
+
+
+
+
+
 ?>
