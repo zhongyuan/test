911b31405dc32323856bec04c12bd85ba34d9c40
owenyang <owenyang@china-liantong.com>
2013-04-07 15:24:49 +0800

    test001

--- protected/views/news/appWinner.php
+++ protected/views/news/appWinner.php
@@ -1,7 +1,28 @@
-<?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/appWinner.css"  />
+<div class="app_dbody">
+	<h2 class="big_title">获奖前言<img src="/images/appPart/add.jpg" align="absmiddle"/></h2>
+	<div class="sec_label">历时一个多月的全国性2013开发者大赛已经圆满结束了，感谢近千名开发者的踊跃参与。经过仔细测试开发者提交的参赛作品，我们在感受到国人强大的实力与各种天马行空的创意之后，总结并评选出以下获奖作品，祝贺他们！！我们会尽快安排奖金及奖品的发放，请获奖者留意邮件或电话通知。</div>
+	
+	<h2 class="big_title">获奖名单<img  src="/images/appPart/peoples.jpg"/></h2>
+	<h3>终极大奖(1名)</h3>
+	<h3>最佳创意奖(2名)</h3>
+	<h3>最佳体验奖(2名)</h3>
+	<h3>最佳设计奖(2名)</h3>
+	<h3>积极参与奖(23名) <span>排名不分先后</span></h3>
+	
+	<p>特别声明</p>
+	<ul class="d_items">
+
+		<li>◆ 任何团体或者个人均可参赛(内部员工除外)，团体和个人皆以提交的作品为单位。</li>
+		<li>◆ 参赛者需在大赛官网注册、报名、提交作品，参赛者报名必须提供真实的姓名、联系方式。如因参赛者提供的信息错误，导致主办方无法与参赛者取得联系，按自动弃权处理。</li>
+		<li>◆ 报名时间：xx年xx月xx日-xx年xx月xx日，逾期提交无效。</li>
+		<li>◆ 参赛作品审核成功后，将发布在大赛官网。最终比赛结果，将综合专家评审团和公众投票意见决定。</li>
+		<li>◆ 比赛结果将在第二届COS开发者大会上公布并颁奖。所有资金为税前所得。</li>
+		<li>◆ 主办方有权对参赛者活动资格进行审核，为核实参赛者的信息，主办方有权要求参赛者提供相关资料以供确认。</li>
+		<li>◆ 参赛者必须保证作品的原创性、合法性，若出现剽窃其他个人或团体所开发应用的情况，将取消参赛资格，所引起的一切法律责任，均由作品开发者自然人或机构团体承担。</li>
+		<li>◆ 在本活动期间，因故不能正常进行时，主办方有权决定取消、终止、修改或暂停活动。</li>
+		<li>◆ 活动最终解释权归主办方所有。</li>
+	</ul>
+	
+</div>
