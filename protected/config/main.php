a5e3bd2d38fe5626236fe8623207d1b43d7f1794
zhongyuan <910398301@qq.com>
2013-04-10 09:43:36 +0800

    css目录

--- protected/config/main.php
+++ protected/config/main.php
@@ -21,12 +21,12 @@ return array(
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
 
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'1',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
+//		'gii'=>array(
+//			'class'=>'system.gii.GiiModule',
+//			'password'=>'1',
+//			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+//			'ipFilters'=>array('127.0.0.1','::1'),
+//		),
 
 	),
 
@@ -56,7 +56,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=cos',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'admin123',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 
