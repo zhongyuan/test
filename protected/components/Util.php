378825ca82458cdbff98eefdd1a53b010d16b9c1
owenyang <owenyang@china-liantong.com>
2013-04-11 17:52:28 +0800

    test001

--- protected/components/Util.php
+++ protected/components/Util.php
@@ -15,7 +15,7 @@ class Util {
         static $conf = array();
         if(!isset($conf[$conf_file_name])){   //导入配置文件，还得用绝对路径,用Yii::app()->request->baseUrl没用
             if(file_exists(dirname(__FILE__).'/../config/cosweb/'.$conf_file_name.'.cfg.php')){
-                $conf[$conf_file_name] = require(dirname(__FILE__).'/../config/orderManage/'.$conf_file_name.'.cfg.php');
+                $conf[$conf_file_name] = require(dirname(__FILE__).'/../config/cosweb/'.$conf_file_name.'.cfg.php');
             }else{
                 echo "不存在该目录";return ;
             }
--- protected/models/MCUsers.php
+++ protected/models/MCUsers.php
@@ -26,17 +26,85 @@ class MCUsers {
     }
 	
 	
-   	public function addUser($email)
+   	public function addUser($input=array())
 	{
-		$sql = "SELECT COUNT(*) AS CNT FROM users WHERE email = :email LIMIT 1";
-		$command = Yii::app()->db->createCommand($sql);
-		$command->bindParam(':email',$email,PDO::PARAM_STR);
-		if($command->queryScalar()){
-			return array(FALSE,"此Email已被其他用户注册了!");
+		if(empty($input)){
+			return array(false,"请填写注册信息");
 		}
 		
-		return array(TRUE,"恭喜您注册成功!");
+		$data = array();
+		$fields = array('user_name','passwd','first_name','last_name');
+		foreach($fields as $ef){
+			if(!isset($input[$ef]) || empty($input[$ef])){
+				return array(FALSE,"注册信息不完整!");
+				break;
+			}else{
+				$data[$ef] = $input[$ef];
+			}
+		}
 		
+		//检查用户是否已被注册
+		if($this->checkUser()){
+			return array(FALSE,"此用户邮箱已被注册，请更换其他邮箱试试吧!");
+		}
+		
+		//详细信息
+		$data['passwd'] = substr(md5($data['passwd']),0,16);//采用16个字符的二进制格式
+		$data['record_time'] = $data['update_time'] = time();
+		$data['status'] = 1;//默认是注册后即激活用户(###后期可考虑只有通过邮件或手机验证后才激活用户###)
+		$data['question_id']=empty($input['question_id'])?0:$input['question_id'];
+		$data['answer'] = empty($input['answer'])?"":$input['answer'];
+		$data['language'] = empty($input['language'])?"zh-cn":$input['language'];
+		
+		if(!empty($input['year']) && !empty($input['month']) && !empty($input['day'])){
+			$data['birthday'] = mktime(0,0,0,intval($input['month']),intval($input['day']),intval($input['year']));
+		}else{
+			$data['birthday'] = 0;
+		}
+		
+		if(!empty($input['viaEmail']) && !empty($input['viaEPaper'])){
+			$data['contact_type'] = 2;//两种联系方式都使用
+		}elseif(!empty($input['viaEmail'])){
+			$data['contact_type'] = 0;//仅使用电子邮件联系
+		}elseif(!empty($input['viaEPaper'])){
+			$data['contact_type'] = 1;//仅使用电子报联系
+		}else{
+			$data['contact_type'] = 100;//不使用任何方式联系此用户
+		}
+		
+		//保存用户信息
+		
+		
+		
+		
+		
+		$affect = $this->_insert("users",$data);
+		if(!$affect){
+			return array(FALSE,"数据写入失败,请重试!");
+		}
+		$user_id = Yii::app()->db->getLastInsertID("users");
+		
+		
+		
+		
+		if(!$user_id){
+			return array(FALSE,"数据写入失败,请重试!");
+		}
+		
+		//保存用户联系地址相关信息
+		$address = array();
+		$addFields = array('country','company','address','county','state','zip_code');
+		foreach($addFields as $af){
+			if(isset($input[$af]) && !empty($input[$af])){
+				$address[$af] = $input[$af];
+			}
+		}
+		
+		$address['update_time'] = $address['record_time'] = time();
+		$address['user_id'] = $user_id;
+		$this->_insert("address",$address);
+		
+		return array(TRUE,$data);//返回用户注册的详细信息
 	} 
 
 	public function deleteUser()
@@ -48,6 +116,34 @@ class MCUsers {
 	{
 		
 	}
+	
+	private function _insert($tbl,$data = array())
+	{
+		if(empty($data)){
+			return FALSE;
+		}
+		
+		$sql = "INSERT INTO $tbl (";
+		$sql.=implode(",",array_keys($data));
+		$sql.=") VALUES(";
+		var_dump(array_values($data));
+		foreach(array_values($data) as $ev){
+			$ev=addslashes($ev);
+			$sql.="'$ev',";
+		}
+		$sql=substr($sql,0,-1);
+		$sql.=")";
+		
+		
+		$command = Yii::app()->db->createCommand($sql);
+		$affetct = $command->execute();
+		if(!$affetct){
+			return FALSE;
+		}
+		return TRUE;
+	}
+	
+	
 
 }
 
