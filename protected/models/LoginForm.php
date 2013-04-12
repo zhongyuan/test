a413d86d217202ece675f13ea699741d1f697201
zhongyuan <910398301@qq.com>
2013-04-12 13:58:16 +0800

    css目录

--- protected/models/LoginForm.php
+++ protected/models/LoginForm.php
@@ -49,8 +49,14 @@ class LoginForm extends CFormModel
 		if(!$this->hasErrors())
 		{
 			$this->_identity=new UserIdentity($this->username,$this->password);
-			if(!$this->_identity->authenticate())
-				$this->addError('password','Incorrect username or password.');
+			if(!$this->_identity->authenticate()){
+                            $language = Yii::app()->session['language'];
+                            if($language=='zh_cn'||!$language){
+                                $this->addError('password','用户名或密码错误.');
+                            }else{
+                                $this->addError('password','Incorrect username or password.');
+                            }
+                        }
 		}
 	}
 
