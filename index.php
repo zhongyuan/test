3e836ceaa691f5f22a7ccdd0c32a56f374edf2b2
zhongyuan <910398301@qq.com>
2013-04-22 13:38:59 +0800

    css目录

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Shanghai");
 
 error_reporting(4); //错误报告等级设置，liang1月13号，不需要@$_GET
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
