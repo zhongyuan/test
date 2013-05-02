261b0a90ceb12837fe720960b952c803d2bad4d7
owenyang <owenyang@china-liantong.com>
2013-05-02 10:19:09 +0800

    test

--- protected/components/views/device.php
+++ protected/components/views/device.php
@@ -3,22 +3,26 @@
 
 <script type="text/javascript">
 jQuery(document).ready(function() {
-    jQuery('#mycarousel').jcarousel();
+    jQuery('#mycarousel').jcarousel({
+		scroll:1
+	});
 });
 </script>
 
 <div class="device_nav">
-	<ul id="mycarousel" class="jcarousel-skin-tango">
-	    <li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_1.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_2.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_3.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_4.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_5.jpg"/></li>
-		
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_1.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_2.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_3.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_4.jpg"/></li>
-		<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_5.jpg"/></li>
-	</ul>
+	<div class="dev_scroller">
+		<ul id="mycarousel" class="jcarousel-skin-tango">
+		    <li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_1.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_2.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_3.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_4.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_5.jpg"/></li>
+			
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_1.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_2.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_3.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_4.jpg"/></li>
+			<li><img src="<?php echo Yii::app()->baseUrl?>/images/device/nav/nav_5.jpg"/></li>
+		</ul>	
+	</div>
 </div>
