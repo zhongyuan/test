4b5b93de7c27c9adffe0e3c9e12c7ad00e8fc21a
owenyang <owenyang@china-liantong.com>
2013-05-02 11:17:41 +0800

    test

--- protected/views/device/pad.php
+++ protected/views/device/pad.php
@@ -2,63 +2,56 @@
 <?php $this->widget('DeviceWidget');?>
 
 <div id="device_box" class="pad_box">
-
-	
-	
-	
-	<ul class="d_phonelist">
-	
-	
-		<li>
-			<div class="img_right pad_one">
-				<h1><span class="green">HTC</span> <span class="black">One Tim</span></h1>
-				<div class="buy_text">
-					<p>作为使用Windows 8系统的多面手，COS One Tim<br />
-					可以在笔记本模式与平板模式间进行切换。与其习一台<br />
-					不完美的半成品不如多花点钱买一台功易用性更高的<br />
-					产品。大声分享您的最爱。
+	<div class="dev_center">
+		<ul class="d_phonelist">
+			<li>
+				<div class="img_right pad_one">
+					<h1><span class="green">HTC <font color="black">One Tim</font></span></h1>
+					<div class="buy_text">
+						<p>作为使用Windows 8系统的多面手，COS One Tim<br />
+						可以在笔记本模式与平板模式间进行切换。与其习一台<br />
+						不完美的半成品不如多花点钱买一台功易用性更高的<br />
+						产品。大声分享您的最爱。
+						</p>
+						<p><input type="button"  name="buy_btn" value="购买" class="buy_btn"/></p>
+					</div>
+				</div>
+				<img  src="<?php echo $this->staticUrl('device/pad/pad_1.jpg');?>" width="300"/>
+			</li>
+			
+			
+			
+			<li class="stick_bottom">
+				<div class="img_left"><img src="<?php echo $this->staticUrl('device/pad/pad_2.jpg');?>" width="400"/></div>
+				<div class="pad_two">
+					<h1 class="orange">HTC BoomSound<sup>TM</sup><br />更清晰、更丰富、更响亮</h1>
+					<p>如果是普通的机顶盒，与朋友分享音乐或视频，效果非常令<br />人沮丧。即便是最激昂的乐曲，微型扬声器也会让它变得沉闷<br />乏味。但全新HTC One的BoomSound<sup>TM</sup>改变了一切。双<br />前置立体扬声器　外加内置扩音器，可提供更响亮、更清晰、<br />更丰富的音效。</p>
 					</p>
-					<p><input type="button"  name="buy_btn" value="购买" class="buy_btn"/></p>
 				</div>
-			</div>
-			<img  src="<?php echo $this->staticUrl('device/pad/pad_1.jpg');?>"/>
-		</li>
-		
-		
-		
-		<li>
-			<div class="img_left"><img src="<?php echo $this->staticUrl('device/pad/pad_2.jpg');?>" width="400"/></div>
-			<div class="pad_two">
-				<h1 class="orange">HTC BoomSound<sup>TM</sup><br />更清晰、更丰富、更响亮</h1>
-				<p>如果是普通的机顶盒，与朋友分享音乐或视频，效果非常令<br />人沮丧。即便是最激昂的乐曲，微型扬声器也会让它变得沉闷<br />乏味。但全新HTC One的BoomSound<sup>TM</sup>改变了一切。双<br />前置立体扬声器　外加内置扩音器，可提供更响亮、更清晰、<br />更丰富的音效。</p>
-				</p>
-			</div>
-		</li>
-		
-		
-		<li>
-			<div class="img_right">
-				<div class="pad_three">
-					<h1 class="green">HTC BinkFeed<sup>TM</sup>您的超炫<br />主屏幕</h1>
-					<p>借助BlinkFeed<sup>TM</sup>，只需挑选出您希望始终保持最新的社交网络、<br />新闻和资讯，它们便可实时更新到您的主屏幕。您的世界，<br />您作主。</p>	
+			</li>
+			
+			
+			<li class="above_bottom">
+				<div class="img_right">
+					<div class="pad_three">
+						<h1 class="green">HTC BinkFeed<sup>TM</sup>您的超炫<br />主屏幕</h1>
+						<p>借助BlinkFeed<sup>TM</sup>，只需挑选出您希望始终保持最新的社交网络、<br />新闻和资讯，它们便可实时更新到您的主屏幕。您的世界，<br />您作主。</p>	
+					</div>
 				</div>
-			</div>
-			<img  src="<?php echo $this->staticUrl('device/pad/pad_3.jpg');?>"/>
-		</li>
-		
-		
-		<li>
-			<div class="img_left"><img src="<?php echo $this->staticUrl('device/pad/pad_4.jpg');?>"/></div>
-			<div class="pad_four">
-				<h1 class="blue">智能平板 更睿智设计</h1>
-				<p>用更创新的方式减少日常磨损？没问题！　HTC One 采用<br />
-					infinity玻璃显示屏，能够有效消除眩光，并且强力抵抗刮擦。<br />
-					无论是现在，还是将来，令人赞叹的外观将会始终光亮如新。
-				</p>
-			</div>
-		</li>
-	</ul>
-
-
-
+				<img  src="<?php echo $this->staticUrl('device/pad/pad_3.jpg');?>" width="400"/>
+			</li>
+			
+			
+			<li class="stick_bottom">
+				<div class="img_left"><img src="<?php echo $this->staticUrl('device/pad/pad_4.jpg');?>"/></div>
+				<div class="pad_four">
+					<h1 class="blue">智能平板 更睿智设计</h1>
+					<p>用更创新的方式减少日常磨损？没问题！　HTC One 采用<br />
+						infinity玻璃显示屏，能够有效消除眩光，并且强力抵抗刮擦。<br />
+						无论是现在，还是将来，令人赞叹的外观将会始终光亮如新。
+					</p>
+				</div>
+			</li>
+		</ul>
+	</div>
 </div>
