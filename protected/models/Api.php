6a6047e4afabc7a4c38ad6688428e24de0a49729
zhongyuan <910398301@qq.com>
2013-07-25 15:44:28 +0800

    ordermanage 管理员

--- protected/models/Api.php
+++ protected/models/Api.php
@@ -1,22 +1,24 @@
 <?php
 
 /**
- * This is the model class for table "newsList".
+ * This is the model class for table "api".
  *
- * The followings are the available columns in table 'newsList':
+ * The followings are the available columns in table 'api':
  * @property integer $id
- * @property string $title
- * @property string $outline
- * @property integer $img_little
+ * @property string $name
+ * @property integer $parent_id
+ * @property string $path
+ * @property integer $status
+ * @property integer $staff_id
  * @property integer $update_time
  * @property integer $record_time
  */
-class NewsList extends CActiveRecord
+class Api extends CActiveRecord
 {
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
-	 * @return NewsList the static model class
+	 * @return Api the static model class
 	 */
 	public static function model($className=__CLASS__)
 	{
@@ -28,7 +30,7 @@ class NewsList extends CActiveRecord
 	 */
 	public function tableName()
 	{
-		return 'newsList';
+		return 'api';
 	}
 
 	/**
@@ -39,13 +41,12 @@ class NewsList extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('title, outline, img_little, update_time, record_time', 'required'),
-			array('img_little, update_time, record_time', 'numerical', 'integerOnly'=>true),
-			array('title', 'length', 'max'=>60),
-			array('outline', 'length', 'max'=>255),
+			array('parent_id, status, staff_id, update_time, record_time', 'numerical', 'integerOnly'=>true),
+			array('name', 'length', 'max'=>100),
+			array('path', 'length', 'max'=>255),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, title, outline, img_little, update_time, record_time', 'safe', 'on'=>'search'),
+			array('id, name, parent_id, path, status, staff_id, update_time, record_time', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -67,9 +68,11 @@ class NewsList extends CActiveRecord
 	{
 		return array(
 			'id' => 'ID',
-			'title' => 'Title',
-			'outline' => 'Outline',
-			'img_little' => 'Img Little',
+			'name' => 'Name',
+			'parent_id' => 'Parent',
+			'path' => 'Path',
+			'status' => 'Status',
+			'staff_id' => 'Staff',
 			'update_time' => 'Update Time',
 			'record_time' => 'Record Time',
 		);
@@ -87,9 +90,11 @@ class NewsList extends CActiveRecord
 		$criteria=new CDbCriteria;
 
 		$criteria->compare('id',$this->id);
-		$criteria->compare('title',$this->title,true);
-		$criteria->compare('outline',$this->outline,true);
-		$criteria->compare('img_little',$this->img_little);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('parent_id',$this->parent_id);
+		$criteria->compare('path',$this->path,true);
+		$criteria->compare('status',$this->status);
+		$criteria->compare('staff_id',$this->staff_id);
 		$criteria->compare('update_time',$this->update_time);
 		$criteria->compare('record_time',$this->record_time);
 
