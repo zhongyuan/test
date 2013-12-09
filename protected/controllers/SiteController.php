0238c038a1afc0acc86de04feca9f380833f2b2c
owenyang <owenyang@china-liantong.com>
2013-12-09 18:15:05 +0800

    register-page-captcha-length-updated

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -12,7 +12,7 @@ class SiteController extends Controller
 			'captcha'=>array(
 				'class'=>'CCaptchaAction',
 				'backColor'=>0xFFFFFF,
-				'maxLength'=>7,
+				'maxLength'=>4,
 			),
 			// page action renders "static" pages stored under 'protected/views/site/pages'
 			// They can be accessed via: index.php?r=site/page&view=FileName
