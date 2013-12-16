e00f8decd7aeaa928d2af8a29d38cb138d8d5ddb
owenyang <owenyang@china-liantong.com>
2013-12-16 16:23:54 +0800

    news-more-btn-removed

--- protected/views/news/index.php
+++ protected/views/news/index.php
@@ -29,8 +29,6 @@
 	                <p class="news_sub_detail">
 						2013年4月1日<br />COS发布会召开<br />各位大虾都非常有兴趣
 					</p>
-
-	                <a href="#"><img class="news_pub_more" src="<?php echo $this->staticUrl('news/index/more.jpg');?>" /></a>
 	            </div>
 
 	        </div>
@@ -42,10 +40,6 @@
 	                <p class="news_sub_detail">
 						最新系统支持平板电脑2.0<br />以上系统升级，并且免费。
 					</p>
-
-	                <a href="#">
-						<img class="news_up_more" src="<?php echo $this->staticUrl('news/index/more.jpg');?>" />
-					</a>
 	            </div>
 	        </div>
 
@@ -59,10 +53,6 @@
 		                <p class="news_sub_detail">
 						五月天的美国<br />可以重游旧地。<br />旅游心情真好。
 						</p>
-
-						<a href="#">
-		                <img class="news_exp_more" src="<?php echo $this->staticUrl('news/index/more.jpg');?>" />
-						</a>
 		               
 		            </div>
 	            </div>
@@ -78,7 +68,7 @@
     <div style="clear:both"></div>
 
     <div id ="replace">
-        <div class="nlist">
+        <!--<div class="nlist">
 			<div class="pleft c_left">
 				<h1><a href="<?php echo $this->createUrl('news/detail');?>">宏达国际电子股份有限公司</a></h1>
 				<p>宏达国际电子股份有限公司成立于1997年5月15日，简称宏达电，亦称HTC，由被誉为台湾的“经营之神”的王永庆之女王雪红任董事长，董事暨宏达基金会董事长卓火土，与总经理兼执行长周永明所创立。</p><p>宏达国际电子股份有限公司，简称宏达电子，品牌为“HTC”。是一家位于台湾的手机与平板电脑制造商。是全球最大的Windows Mobile智能手机生产厂商，全球最大的智能手机代工和生产厂商。</p>
@@ -107,7 +97,31 @@
 			</div>
 			<img src="<?php echo $this->staticUrl('news/index/news_images_6.jpg');?>" class="rgt"/>
 			<div><img src="<?php echo $this->staticUrl('div_split.jpg');?>"/></div>
-		</div>
+		</div>-->
+		<?php 
+			 $idx = 0;
+			 foreach($models as $m):
+				$idx++;
+				if($idx%2 == 0):?>
+				<div class="nlist">
+					<img src="<?php echo $m->image_name;?>" class="lft"/>
+					<div class="pright">
+						<h1><a href="<?php echo $this->createUrl('news/detail',array('news_id'=>$m->id));?>" target="_blank"><?php echo $m->title;?></a></h1>
+						<p><?php echo $m->outline;?></p>
+					</div>
+					<div><img src="<?php echo $this->staticUrl('div_split.jpg');?>"/></div>
+		        </div>
+		<?php	else:?>
+				<div class="nlist">
+					<div class="pleft c_left">
+						<h1><a href="<?php echo $this->createUrl('news/detail',array('news_id'=>$m->id));?>" target="_blank"><?php echo $m->title;?></a></h1>
+						<p><?php echo $m->outline;?></p>
+					</div>
+					<img src="<?php echo $m->image_name;?>" class="rgt"/>
+					<div><img src="<?php echo $this->staticUrl('div_split.jpg');?>"/></div>
+		        </div>
+		<?php	endif;?>
+		<?php endforeach;?>
 		
 <!-- ===========================翻页====================== -->
 
