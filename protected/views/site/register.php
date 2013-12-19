345b5ab166867408c700953e82a9b53eec2db3f8
owenyang <owenyang@china-liantong.com>
2013-12-19 17:56:03 +0800

    resize-open-window

--- protected/views/site/register.php
+++ protected/views/site/register.php
@@ -474,7 +474,7 @@ function openwindow(url,name,iWidth,iHeight)
   var iHeight;                        //弹出窗口的高度;
   var iTop = (window.screen.availHeight-30-iHeight)/2;       //获得窗口的垂直位置;
   var iLeft = (window.screen.availWidth-10-iWidth)/2;           //获得窗口的水平位置;
-  window.open(url,name,'height='+iHeight+',,innerHeight='+iHeight+',width='+iWidth+',innerWidth='+iWidth+',top='+iTop+',left='+iLeft+',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
+  window.open(url,name,'height='+iHeight+',,innerHeight='+iHeight+',width='+iWidth+',innerWidth='+iWidth+',top='+iTop+',left='+iLeft+',toolbar=no,menubar=no,scrollbars=yes,resizeable=yes,location=no,status=no');
 }
 
 </script>
