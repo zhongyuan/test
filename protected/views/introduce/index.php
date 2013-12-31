b4d597bbee44c4588fcee9ce39910b067e4870e9
zhongyuan <910398301@qq.com>
2013-12-31 18:11:02 +0800

    开发者指引图片重新添加

--- protected/views/introduce/index.php
+++ protected/views/introduce/index.php
@@ -1,36 +1,61 @@
-
-
 <style>
+    .div_height1{
+        height: 648px;
+    }
+    .div_height2{
+        height: 488px;
+    }
+    .div_height3{
+        height: 675px;
+    }
+    .div_height4{
+        height: 761px;
+    }
     .Icenter{
         width: 980px;
         margin: 0 auto;
     }
     .imgbg{
-        background-color: #f7f5f2;
+        background-color: #ffffff;
     }
 </style>
 
-
-<div class="Iindexbg">
-    <div>
-        <div class="Icenter">
-            <img src="<?php echo $this->staticUrl('introduce/platform1.jpg'); ?>">
+<div class="outstanding_bg">
+    <div class="dd div_height1">
+        <div class="mm">
+            <div class="ff">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_1.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_2.jpg'); ?>">
+            </div>
         </div>
     </div>
     <div class="imgbg">
         <div class="Icenter">
-            <img src="<?php echo $this->staticUrl('introduce/platform2.jpg'); ?>">
+            <img src="<?php echo $this->staticUrl('introduce/compatible_3.jpg'); ?>">
         </div>
     </div>
-    <div>
-        <div class="Icenter">
-            <img src="<?php echo $this->staticUrl('introduce/platform3.jpg'); ?>">
+    <div class="dd div_height2">
+        <div class="mm">
+            <div class="ff">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_4.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_5.jpg'); ?>">
+            </div>
         </div>
     </div>
-    <div class="imgbg">
-        <div class="Icenter">
-            <img src="<?php echo $this->staticUrl('introduce/platform4.jpg'); ?>">
+    <div class="dd div_height3">
+        <div class="mm">
+            <div class="ff">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_6.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_7.jpg'); ?>">
+            </div>
         </div>
     </div>
-
+    <div class="dd div_height4">
+        <div class="mm">
+            <div class="ff">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_8.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('introduce/compatible_9.jpg'); ?>">
+            </div>
+        </div>
+    </div>
 </div>
---
+++ protected/views/introduce/outstanding.php
@@ -0,0 +1,41 @@
+<style>
+    .div_height1{
+        height: 611px;
+    }
+    .div_height2{
+        height: 557px;
+    }
+    .Icenter{
+        width: 980px;
+        margin: 0 auto;
+    }
+    .imgbg{
+        background-color: #f8f8f8;
+    }
+</style>
+
+<div class="outstanding_bg">
+    <div class="dd div_height1">
+        <div class="mm">
+            <div class="ff">
+                <img src="<?php echo $this->staticUrl('introduce/outstanding_1.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('introduce/outstanding_2.jpg'); ?>">
+            </div>
+        </div>
+    </div>
+
+    <div class="dd div_height2">
+        <div class="mm">
+            <div class="ff">
+                <img src="<?php echo $this->staticUrl('introduce/outstanding_3.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('introduce/outstanding_4.jpg'); ?>">
+            </div>
+        </div>
+    </div>
+    <div class="imgbg">
+        <div class="Icenter">
+            <img src="<?php echo $this->staticUrl('introduce/outstanding_5.jpg'); ?>">
+            <img src="<?php echo $this->staticUrl('introduce/outstanding_6.jpg'); ?>">
+        </div>
+    </div>
+</div>
