778e52561f8f7df353218eb4ce7d2649b20743bd
zhongyuan <910398301@qq.com>
2013-12-31 16:29:21 +0800

    重构删除垃圾代码

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -7,9 +7,11 @@ $this->breadcrumbs=array(
 	'Error',
 );
 ?>
-
+<div style="min-height: 600px;">
 <h2>Error <?php echo $code; ?></h2>
 
 <div class="error">
 <?php echo CHtml::encode($message); ?>
+</div>
+
 </div>
