9c58134d24cb57ea8400e948bf9aa873619bccf0
zhongyuan <910398301@qq.com>
2014-01-06 15:41:55 +0800

    Merge origin/master
    Conflicts:
    	protected/components/views/footer.php

--- protected/components/views/footer.php
+++ protected/components/views/footer.php
@@ -9,16 +9,14 @@
 </style>
 <div style="background-color: rgb(255, 173, 47);height: 2px;width: 100%;border: 1px;"></div>
 <div class="footer">
-    <div>
-        <div class="footer_info text_center">
-                <a href="<?php echo Yii::app()->createUrl('about/index'); ?>"><span>关于我们</span></a>
-                <a href="<?php echo Yii::app()->createUrl('about/contact'); ?>"><span>联系我们</span></a>
-                <a href="<?php echo Yii::app()->createUrl('news/index'); ?>"><span>最新动态</span></a>
-                <a href="http://developer.china-cos.com/signup/"><span>开发者注册</span></a>
-                <a href="http://developer.china-cos.com/user/"><span>开发者管理</span></a>
-                <a href="<?php echo Yii::app()->createUrl('site/privacy'); ?>"><span>服务条款</span></a>
+    <div class="footer_info text_center">
+            <a href="<?php echo Yii::app()->createUrl('about/index'); ?>"><span>关于我们</span></a>
+            <a href="<?php echo Yii::app()->createUrl('about/contact'); ?>"><span>联系我们</span></a>
+            <a href="<?php echo Yii::app()->createUrl('news/index'); ?>"><span>最新动态</span></a>
+            <a href="http://developer.china-cos.com/signup/"><span>开发者注册</span></a>
+            <a href="http://developer.china-cos.com/user/"><span>开发者管理</span></a>
+            <a href="<?php echo Yii::app()->createUrl('site/privacy'); ?>"><span>服务条款</span></a>
 
-        </div>
     </div>
 
     <div style="text-align: center;padding: 20px 0;">
