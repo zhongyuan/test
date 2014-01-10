e523718a8495fdbdb9021505707acc7ae5324392
zhongyuan <910398301@qq.com>
2014-01-10 13:46:37 +0800

    首页图片

--- protected/views/about/contact_old.php
+++ protected/views/about/contact_old.php
@@ -28,18 +28,18 @@
     }
     .contact_row_left{
         /*width: 240px;*/
-        width: 290px;
+        width: 250px;
         /*display: inline-block;*/
         float: left;
     }
     .contact_row_right{
-        width: 680px;
+        width: 725px;
         /*display: inline-block;*/
         float: left;
     }
     .contact_row_right li{
         /*width: 235px;*/
-        width: 290px;
+        width: 210px;
         /*display: inline-block;*/
         float: left;
     }
@@ -72,15 +72,8 @@
         margin-bottom: 20px;
     }
     .row_margin{
-        /*margin: auto 40px;*/
-        margin-left: 40px;
+        margin: auto 40px;
         /*margin: auto 5px;*/
-    }
-    .float{
-        float: left;
-    }
-    .margin_top{
-        margin-top: 40px;
     }
 </style>
 
@@ -116,6 +109,14 @@
                         Recruit@china-liantong.com
                     </div>
                 </li>
+<!--                <li>
+                    <div class="commerce_title"><span class="black_icon"></span><span>产业链相关</span></div>
+                    <div>
+                        李先生<br/>
+                        l33@china-liantong.com<br/>
+                        上海市浦东新区博霞路11号
+                    </div>
+                </li>-->
             </ul>
         </div>
     </div>
@@ -127,39 +128,27 @@
             <div class="contact_rowimg crowimg_posi2"></div>
         </div>
         <div class="contact_row_right">
-            <div class="float">
-                <ul>
-                    <li>
-                        <div class="commerce_title"><span class="black_icon"></span><span>移动设备合作</span></div>
-                        <div>
-                            Mobile@china-liantong.com
-                        </div>
-                    </li>
-                    <li class="row_margin">
-                        <div class="commerce_title"><span class="black_icon"></span><span>智能机顶盒合作</span></div>
-                        <div>
-                            OTT@china-liantong.com
-                        </div>
-                    </li>
-
-                </ul>
-            </div>
-            <div class="float margin_top">
-                <ul>
-                    <li>
-                        <div class="commerce_title"><span class="black_icon"></span><span>应用合作(移动终端)</span></div>
-                        <div>
-                            MobileAPP@china-liantong.com
-                        </div>
-                    </li>
-                    <li class="row_margin">
-                        <div class="commerce_title"><span class="black_icon"></span><span>应用合作(电视及机顶盒)</span></div>
-                        <div>
-                            TVAPP@china-liantong.com
-                        </div>
-                    </li>
-                </ul>
-            </div>
+            <ul>
+                <li>
+                    <div class="commerce_title"><span class="black_icon"></span><span>移动设备合作</span></div>
+                    <div>
+                        Mobile@china-liantong.com
+                    </div>
+                </li>
+                <li class="row_margin">
+                    <div class="commerce_title"><span class="black_icon"></span><span>智能机顶盒合作</span></div>
+                    <div>
+                        OTT@china-liantong.com
+                    </div>
+                </li>
+                <li>
+                    <div class="commerce_title"><span class="black_icon"></span><span>应用商店合作</span></div>
+                    <div>
+                        MobileAPP@china-liantong.com<br/>
+                        TVAPP@china-liantong.com
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
     
