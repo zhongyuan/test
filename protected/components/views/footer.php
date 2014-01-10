6f7d63fe6abfbb398c62a17146c62f91439cd411
zhongyuan <910398301@qq.com>
2014-01-10 16:58:38 +0800

    首页图片

--- protected/components/views/footer.php
+++ protected/components/views/footer.php
@@ -2,7 +2,7 @@
 <div style="background-color: rgb(255, 173, 47);height: 2px;width: 100%;border: 1px;"></div>
 <div class="footer">
     <div class="footer_info text_center">
-            <span>© 上海联彤网络通讯技术有限公司 版权所有</span>
+            <span>© 上海联彤网络通讯技术有限公司版权所有</span>
             <a href="<?php echo Yii::app()->createUrl('about/index'); ?>"><span>联系我们</span></a>
             <a href="<?php echo Yii::app()->createUrl('site/privacy'); ?>"><span>服务条款</span></a>
     </div>
