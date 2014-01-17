320dfd7a4ada7cb38f625da7c506edcff6bbc48f
zhongyuan <910398301@qq.com>
2014-01-17 23:16:55 +0800

    调本地视频源,添加手机播放页

--- protected/views/site/playVideo.php
+++ protected/views/site/playVideo.php
@@ -1,6 +1,7 @@
 <style>
     .playVideo_bg{
         width: 980px;
+        height: 1000px;
         margin: 0 auto;
         overflow: hidden;
     }
@@ -10,13 +11,5 @@
     }
 </style>
 <div class="playVideo_bg">
-    <div class="video">
-            <object id="fancybox-swf" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="100%" height="100%">
-                <param name="movie" value="http://sm.resource.china-cos.com/player.swf?type=http&amp;file=cos_intro.flv">
-                <param name="wmode" value="transparent"><param name="allowfullscreen" value="true">
-                <param name="allowscriptaccess" value="always">
-                <embed src="http://sm.resource.china-cos.com/player.swf?type=http&amp;file=cos_intro.flv" type="application/x-shockwave-flash" 
-                       width="100%" height="100%" wmode="transparent" allowfullscreen="true" allowscriptaccess="always">
-            </object>
-    </div>
+
 </div>
