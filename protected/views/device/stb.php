c2b82e21ec0266fda2584121345db4863719c8f2
zhongyuan <910398301@qq.com>
2014-01-20 17:11:55 +0800

    上线后，第一次页面修改

--- protected/views/device/stb.php
+++ protected/views/device/stb.php
@@ -2,14 +2,14 @@
     .div_height1{
         height:486px;
     }
-    .div_height2{
-        height:658px;
+	.div_height2{
+      height:702px;
     }
 	.div_height3{
-        height:472px;
+       height:467px;
     }
 	.div_height4{
-        height:541px;
+       height:546px;
     }
     .imgbg{
         background-color: #5793f3;
@@ -22,36 +22,39 @@
         margin: 0 auto;
         overflow: hidden;
     }
+	
 </style>
 
 <div class="mterminalbg">
     <div class="imgbg div_height1">
         <div class="Icenter">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_1.jpg'); ?>">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_2.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_1.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_2.jpg'); ?>">
         </div>
     </div>
-    <div class="dd div_height2">
+	
+	<div class="dd div_height2">
         <div class="mm">
             <div class="ff">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_3.jpg'); ?>">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_4.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_3.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_4.jpg'); ?>">
             </div>
         </div>
-    </div>
+    </div>	
+   
 
     <div class="imgbg2 div_height3">
         <div class="Icenter">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_5.jpg'); ?>">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_6.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_5.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_6.jpg'); ?>">
         </div>
     </div>
 
 	<div class="dd div_height4">
         <div class="mm">
             <div class="ff">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_7.jpg'); ?>">
-                <img src="<?php echo $this->staticUrl('device/telebox_new2/smart_tv_8.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_7.jpg'); ?>">
+                <img src="<?php echo $this->staticUrl('device/telebox_new3/smart_tv_8.jpg'); ?>">
             </div>
         </div>
     </div>
