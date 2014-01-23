e4bd23d5c2c3799a3861550d396cbd55feae107e
zhongyuan <910398301@qq.com>
2014-01-23 11:56:10 +0800

    合作伙伴清除注释文件

--- protected/controllers/PartnerController.php
+++ protected/controllers/PartnerController.php
@@ -28,46 +28,6 @@ class PartnerController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$this->actionEcology();
-        //$this->render('ecology');  //暂时隐藏掉产业链
-
-//		$criteria = new CDbCriteria(array(
-//            'select' => 'id,title,outline,image_name',
-//            'order' => 'update_time desc',
-//            'params' => array(
-//                ':type' => 1, //第二种类型
-//                ':status' => 0, //新闻正常，没有被屏蔽
-//            ),
-//        ));
-//         $count=  NewsList::model()->count($criteria);
-//         $pages=new CPagination($count);
-//
-//         $pages->pageSize=4;
-//         $pages->applyLimit($criteria);
-//         $models = NewsList::model()->findAll($criteria);
-//
-//         //第一种方法:判断请求
-//        if (Yii::app()->request->isAjaxRequest) {
-//            $this->renderPartial('_industry',array(
-//                'models' => $models,
-//                'pages' => $pages,
-//            ));
-//            exit;
-//        }
-//
-//        $this->render('industry', array(
-//           'models' => $models,
-//           'pages' => $pages,
-//        ));
-        }
-
-
-	/**
-	 * 合作伙伴 - 生态链 
-	 * 
-	 */
-	public function actionEcology()
-	{
 		$mcPartner = new MCPartner();
 		$dbLogo = $mcPartner->getParterList();
 		
@@ -88,7 +48,17 @@ class PartnerController extends Controller
 				),
 				'dbLogo' => $dbLogo
         ));
-	}
+    }
+
+
+	/**
+	 * 合作伙伴 - 生态链 
+	 * 
+	 */
+//	public function actionEcology()
+//	{
+//
+//	}
 	
 	
 	/**
